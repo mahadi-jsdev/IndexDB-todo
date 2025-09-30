bc97c64803dcecd38c3350c91a973127362a0763
mahadi-init <init.mahadi@gmail.com>
2025-09-30 14:14:17 +0600

    Updated src/components/TodoApp.tsx

--- src/components/TodoApp.tsx
+++ src/components/TodoApp.tsx
@@ -485,7 +485,7 @@ const TodoApp = () => {
                 Ongoing ({filteredTodos('ongoing').length})
               </CardTitle>
             </CardHeader>
-            <ScrollArea className="h-[500px] rounded-md border">
+            <ScrollArea className="h-[550px] rounded-md border">
               <CardContent className="p-4 space-y-3">
                 {filteredTodos('ongoing').map(todo => (
                   <div key={todo.id} className="bg-white p-3 rounded-lg border border-yellow-200 shadow-sm">
@@ -563,7 +563,7 @@ const TodoApp = () => {
                 Done ({filteredTodos('done').length})
               </CardTitle>
             </CardHeader>
-            <ScrollArea className="h-[500px] rounded-md border">
+            <ScrollArea className="h-[550px] rounded-md border">
               <CardContent className="p-4 space-y-3">
                 {filteredTodos('done').map(todo => (
                   <div key={todo.id} className="bg-white p-3 rounded-lg border border-green-200 shadow-sm">
